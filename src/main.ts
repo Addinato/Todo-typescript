5e78d1ae5aeffd95bf101a13181f3e7d9b73f2d6
Adrian Lydén <adrianlyden@gmail.com>
2025-11-04 14:33:01 +0100

    adrian fuckar runt

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,52 @@
 import './style.css'
 
+
+// const theForm = document.getElementById("theForm") as HTMLFormElement;
+
+// theForm.addEventListener("submit", handleSubmit);
+
+// let toDo = JSON.parse(localStorage.getItem("toDo") || "[]" ) as TodoTask[];
+
+// function saveList (){
+//     localStorage.setItem("toDo", JSON.stringify(toDo));
+// }
+
+interface ToDoItem {
+    text: string;
+    done: boolean;
+  }
+  
+  const toDo: ToDoItem[] = [];
+  
+  const toDoList = document.getElementById("toDoList") as HTMLUListElement;
+  const addButton = document.getElementById("addButton") as HTMLButtonElement;
+  const input = document.getElementById("input") as HTMLInputElement;
+  
+  // Förhindrar att sidan laddas om när du klickar på knappen i formuläret
+
+  const form = document.getElementById("theForm") as HTMLFormElement;
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  
+  addButton.onclick = function () {
+    const value = input.value.trim();
+  
+    if (value !== "") {
+      // Lägg till i arrayen
+      toDo.push({ text: value, done: false });
+  
+      // Skapa nytt <li>
+      const item = document.createElement("li");
+      item.textContent = value;
+  
+      // Lägg till i listan
+      toDoList.appendChild(item);
+  
+      // Töm input
+      input.value = "";
+    }
+  };
+  
+
+
